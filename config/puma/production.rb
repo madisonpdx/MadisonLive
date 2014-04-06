ab16eba3eb201f302845e9f7998db792705ad316
Mark Kanof <mark@swervelabs.com>
2014-04-05 21:00:14 -0700

    Puma config.

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,9 +3,9 @@ environment 'production'
 threads 2,16
 workers 2
 
-pidfile '/var/www/madisonlive/shared/pids/puma.pid'
-state_path '/var/www/madisonlive/shared/sockets/puma.state'
-bind 'unix:///var/www/madisonlive/shared/sockets/puma.sock'
-activate_control_app 'unix:///var/www/madisonlive/shared/sockets/pumactl.sock'
+pidfile '/var/www/madison_live/shared/pids/puma.pid'
+state_path '/var/www/madison_live/shared/sockets/puma.state'
+bind 'unix:///var/www/madison_live/shared/sockets/puma.sock'
+activate_control_app 'unix:///var/www/madison_live/shared/sockets/pumactl.sock'
 
 daemonize
