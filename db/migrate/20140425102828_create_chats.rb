584f9b3fcbade2c09ac522384c6878df375943a3
Tristan Damron <TristanDamron31@gmail.com>
2014-04-25 04:31:13 -0700

    Edited Links to Messages

--- db/migrate/20140425102828_create_chats.rb
+++ db/migrate/20140425102828_create_chats.rb
@@ -1,8 +1,19 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
-
-      t.timestamps
+    t.string :sender_id, :null => false
+	t.string :recepient_id
+	t.string :date
+	t.boolean :sender_deleted, :recepient_deleted, :default => 0
+	t.string :subject, :null => false
+	t.text :body
+	t.string :container, :default => "draft"
+    t.timestamps
     end
   end
+
+def down 
+	drop_table :messages
+	end
+end
 end
