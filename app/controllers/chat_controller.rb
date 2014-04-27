1ae37adaf00283d438716b6c1ff79825ce31360f
Tristan Damron <TristanDamron31@gmail.com>
2014-04-27 06:16:05 -0700

    Added Mailboxer Gem

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,11 @@
 class ChatController < ApplicationController
 	def show
+		@user = User.find(params[:id])
+		@message = current_user.messages.new
+	end
 
+	def create
+		@user = User.find(params[:id])
+		@message = current_user.connections.build(params[:message])
 	end
 end
