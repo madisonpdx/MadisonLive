a594e449b7f9e2bc190f70250c5c0e10c667d62f
Mark Kanof <mark@kanof.com>
2014-05-20 13:41:15 -0700

    Remove and restore Bootstrap to fix issues. Reformat layout template.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    if self.first_name.present? and self.last_name.present?
+      self.first_name + ' ' + self.last_name
+    elsif self.last_name.present?
+      self.last_name
+    elsif self.first_name.present?
+      self.first_name
+    else
+      'Stranger'
+    end
+  end
 end
