7d6d39dd31382c27a15ea4c36e1b49b9f4b59a03
Mark Kanof <mark@kanof.com>
2014-05-31 11:38:26 -0700

    Add password and email edit to edit profile. Allow sending of direct messages.

--- app/controllers/generalsettings_controller.rb
+++
@@ -1,4 +0,0 @@
-class GeneralsettingsController < ApplicationController
-  def show
-  end
-end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,9 +11,11 @@ class ProfilesController < ApplicationController
 
   def update
     @user = current_user
-    @user.update(params.require(:user).permit(:first_name, :last_name, :avatar))
+    @user.update(params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar))
 
     if @user.save
+      sign_in @user, :bypass => true
+
       redirect_to profile_path
     else
       render 'profiles/edit'
